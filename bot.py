ac688eb3de73d28ada3adde95aefb5c3228b1036
Aniruddh Goteti <aniruddh.goteti@blueyonder.com>
2020-11-06 20:25:03 +0100

    Add different agents to code

--- bot.py
+++ bot.py
@@ -1,26 +1,31 @@
 import gym
 from beer_game_env.envs import BeerGame
 import numpy as np
+from enum import Enum, auto
 
+class AgentType(Enum):
+    Retailer = auto()
+    Wholesale = auto()
+    Manufacturer = auto()
+    Factory = auto()
 
-class Agent:
-
-    def player_actions(self):
-        "Return 4 actions as numpy array for each agent: brewery, distributor, wholesaler, retailer."
-        "This function can be modified or new functions can also be introduced in Agent class"
-        "by competitor."
-        actions = np.random.uniform(0, 16, size=4)
-        actions = actions.astype(int)
-        return actions
+class Agent(object):
+    def __init__(self, agent_type: AgentType):
+         self.agent_type = agent_type
+    
+    def get_action(self, step_state: dict):
+        return np.random.randint(0, 4)  # random order
+        # your decision logic goes there!
 
 
 if __name__ == '__main__':
     env = BeerGame(env_type='classical') #TODO: decide if we should support all 3 environments or support one
     env.reset()
     done = False
+    agents = [Agent(AgentType.Retailer), Agent(AgentType.Wholesale), Agent(AgentType.Manufacturer), Agent(AgentType.Factory)]
+    step_state = env.reset()
     while not done:
         env.render()
-        agent = Agent()
-        actions = agent.player_actions() # returns 4 random int as numpy array as of now
+        actions = [a.get_action(step_state[i]) for i, a in enumerate(agents)]
         step_state, step_rewards, done, _ = env.step(actions)
 
