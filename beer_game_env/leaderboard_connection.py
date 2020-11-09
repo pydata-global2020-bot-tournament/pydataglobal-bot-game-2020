05ff8f00fd035bcc123f0d8f0b8a6dddb69e1bca
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-11-09 22:01:41 +0100

    get git user

--- beer_game_env/leaderboard_connection.py
+++ beer_game_env/leaderboard_connection.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -6,12 +8,15 @@ def post_score_to_api(score: float):
     # authentication
     username = "pydata-leaderboatd-api-user"
     password = "svjYxP6P3Tpnp4meK32N4ZwN22H8ddD5xg4hXrhf"
-
+    url = "https://leaderboard-server.pydata-bot-tournament.eu.live.external.byp.ai/add-user-score"
+    
     # payload data
     data = {"user": "Player1", "score": score}
-    
+    git_user = os.system("git log -1 --pretty=format:'%an'")
+    print(f"got user: {git_user}")
+    print(f"got score: {score}")
     r = requests.post(
-        url="http://127.0.0.1:8000/add-user-score",
+        url=url,
         json=data,
         headers={"content-type": "application/json"},
         auth=HTTPBasicAuth(username, password),    
