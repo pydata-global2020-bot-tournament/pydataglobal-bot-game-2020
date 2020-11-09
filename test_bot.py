629f086655f281b35461ba761cebd81844f3811d
aniruddh-goteti-by <aniruddh.goteti@blueyonder.com>
2020-11-09 11:27:00 +0100

    Add test

--- test_bot.py
+++ test_bot.py
@@ -1,30 +1,22 @@
-import gym
+from bot import AgentType, Agent
 from beer_game_env.envs import BeerGame
 import numpy as np
-from enum import Enum, auto
 
-class AgentType(Enum):
-    Retailer = auto()
-    Wholesaler = auto()
-    Distributor = auto()
-    Manufacturer = auto()
-
-class Agent(object):
-    def __init__(self, agent_type: AgentType):
-         self.agent_type = agent_type
-    
+class TestAgent(Agent):
+    def __init__(self, AgentType):
+        self.counter = 0
     def get_action(self, step_state: dict):
-        return np.random.randint(0, 4)  # random order
-        # your decision logic goes there!
+        self.counter += 1
+        return 4  # always the same action, 4
 
 
-if __name__ == '__main__':
+def test_bot():
     env = BeerGame(env_type='classical') #TODO: decide if we should support all 3 environments or support one
     done = False
-    agents = [Agent(t) for t in AgentType]
+    agents = [TestAgent(t) for t in AgentType]
     step_state = env.reset()
     while not done:
         env.render()
         actions = [a.get_action(step_state[i]) for i, a in enumerate(agents)]
         step_state, step_rewards, done, _ = env.step(actions)
-
+        assert actions == [4,4,4,4]
