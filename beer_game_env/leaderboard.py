8e43a720877e9dffbb09f13940175f2242ad44cc
Lioman <dev@lioman.de>
2020-11-10 15:16:53 +0100

    Add BY CA to verify requests

--- beer_game_env/leaderboard.py
+++ beer_game_env/leaderboard.py
@@ -3,25 +3,31 @@ import subprocess
 
 import requests
 from requests.auth import HTTPBasicAuth
+from pathlib import Path
 
 
 def post_score_to_api(score: float):
     # payload data
     data = {"user": "Player1", "score": score}
-    git_user = subprocess.check_output(["git", "log", "-1", "--pretty=format:%an"]).decode()
+    git_user = subprocess.check_output(
+        ["git", "log", "-1", "--pretty=format:%an"]
+    ).decode()
     print(f"Sending data to leaderboard: User: {git_user}, score: {score}")
 
     # authentication
-    username = os.environ['LEADERBOARD_API_USERNAME']
-    password = os.environ['LEADERBOARD_API_PASSWORD']
+    username = os.environ["LEADERBOARD_API_USERNAME"]
+    password = os.environ["LEADERBOARD_API_PASSWORD"]
     url = "https://leaderboard-server.pydata-bot-tournament.eu.live.external.byp.ai/add-user-score"
+
+    ca_file = str(Path(__file__).parent.absolute() / "resources" / "CA.crt")
 
     r = requests.post(
         url=url,
         json=data,
         headers={"content-type": "application/json"},
-        auth=HTTPBasicAuth(username, password),    
+        auth=HTTPBasicAuth(username, password),
+        verify=ca_file,
     )
 
     assert r.ok
-    print(f"post was successful")
+    print("post was successful")
