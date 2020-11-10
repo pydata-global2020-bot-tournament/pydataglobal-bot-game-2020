af040297bfaaddc0165d9e473152d666338bc6cc
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-11-10 14:27:38 +0100

    read credentials from github secrets

--- beer_game_env/leaderboard.py
+++ beer_game_env/leaderboard.py
@@ -1,20 +1,21 @@
+import os
 import subprocess
+
 import requests
 from requests.auth import HTTPBasicAuth
 
 
-def post_score_to_api(score: float):  
-    # authentication
-    username = "pydata-leaderboatd-api-user"
-    password = "svjYxP6P3Tpnp4meK32N4ZwN22H8ddD5xg4hXrhf"
-    url = "https://leaderboard-server.pydata-bot-tournament.eu.live.external.byp.ai/add-user-score"
-
+def post_score_to_api(score: float):
     # payload data
     data = {"user": "Player1", "score": score}
     git_user = subprocess.check_output(["git", "log", "-1", "--pretty=format:%an"]).decode()
-
-    # git_user = os.system("git log -1 --pretty=format:'%an'")
     print(f"Sending data to leaderboard: User: {git_user}, score: {score}")
+
+    # authentication
+    username = os.environ['LEADERBOARD_API_USERNAME']
+    password = os.environ['LEADERBOARD_API_PASSWORD']
+    url = "https://leaderboard-server.pydata-bot-tournament.eu.live.external.byp.ai/add-user-score"
+
     r = requests.post(
         url=url,
         json=data,
@@ -23,3 +24,4 @@ def post_score_to_api(score: float):
     )
 
     assert r.ok
+    print(f"post was successful")
