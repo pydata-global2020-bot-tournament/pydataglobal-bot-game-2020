764c3f8cfca6f38b49a0128cd84505ab262788a0
Lioman <dev@lioman.de>
2020-11-13 11:49:54 +0100

    add github user from commit data
    Since commit name and github user are not always the same,
    we fetch commit info from github first.
    The data score will be updated with this one, if we get a response

--- supply_chain_env/leaderboard.py
+++ supply_chain_env/leaderboard.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from pathlib import Path
+from typing import Optional
 
 import requests
 from requests.auth import HTTPBasicAuth
@@ -9,11 +10,13 @@ from requests.auth import HTTPBasicAuth
 def post_score_to_api(score: float):
     # payload data
     data = {"user": "Anonymous", "score": score}
-    git_user = subprocess.check_output(
-        ["git", "log", "-1", "--pretty=format:%an"]
+    git_info = subprocess.check_output(
+        ["git", "log", "-1", "--pretty=format:%H, %an"]
     ).decode()
-    if git_user:
-        data["user"] = git_user
+    ref, git_user = git_info.split(",")
+    github_user = _get_github_user(ref)
+    data["user"] = github_user if github_user else git_user
+
     print(f"Sending data to leaderboard: User: {git_user}, score: {score}")
 
     # authentication
@@ -36,3 +39,17 @@ def post_score_to_api(score: float):
             f"Updating score was not successful and failed with {r.status_code}"
         )
     print("post was successful")
+
+
+def _get_github_user(ref: str) -> Optional[str]:
+    try:
+        url = f"https://api.github.com/repos/pydata-global2020-bot-tournament/pydataglobal-bot-game-2020/commits/{ref}"
+        r = requests.get(url)
+        if r.ok:
+            commit = r.json()
+            return commit.get("author", {}).get("login")
+        return None
+    except Exception as e:
+        print("Can not get data from github api")
+        print(e)
+        return None
